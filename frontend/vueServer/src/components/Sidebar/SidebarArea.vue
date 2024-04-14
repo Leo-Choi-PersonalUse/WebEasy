345ab5547fc5af8a30fe0783ea54f7fa154bbed7
leochoifse <leochoi@fseng.com.hk>
2024-04-14 21:53:11 +0800

    add reslover for laravel

--- frontend/vueServer/src/components/Sidebar/SidebarArea.vue
+++ frontend/vueServer/src/components/Sidebar/SidebarArea.vue
@@ -344,7 +344,7 @@ const menuGroups = ref([
       <!-- Sidebar Menu -->
 
       <!-- Promo Box -->
-      <div
+      <!-- <div
         class="mx-auto mb-10 w-full max-w-60 rounded-sm border border-strokedark bg-boxdark py-6 px-4 text-center shadow-default"
       >
         <h3 class="mb-1 font-semibold text-white">TailAdmin Pro</h3>
@@ -357,7 +357,7 @@ const menuGroups = ref([
         >
           Purchase Now
         </a>
-      </div>
+      </div> -->
       <!-- Promo Box -->
     </div>
   </aside>
