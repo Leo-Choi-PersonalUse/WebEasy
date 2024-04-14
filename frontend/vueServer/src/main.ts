e93af8ad7c41984b1126a6194eb6d0c25371dad0
leochoifse <leochoi@fseng.com.hk>
2024-04-14 20:20:07 +0800

    add tailadmin to frontend

--- frontend/vueServer/src/main.ts
+++ frontend/vueServer/src/main.ts
@@ -1,7 +1,11 @@
-import './assets/main.css'
+import './assets/css/satoshi.css'
+import './assets/css/style.css'
+import 'jsvectormap/dist/css/jsvectormap.min.css'
+import 'flatpickr/dist/flatpickr.min.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import VueApexCharts from 'vue3-apexcharts'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +14,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueApexCharts)
 
 app.mount('#app')
