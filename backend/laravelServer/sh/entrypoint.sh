0cad973e1facdf1e5a8bbbb8df29a37cc1e0a08a
Leo Choi <leochoi@fseng.com.hk>
2024-10-09 15:26:12 +0800

    dev

--- backend/laravelServer/sh/entrypoint.sh
+++ backend/laravelServer/sh/entrypoint.sh
@@ -3,27 +3,27 @@
 set -e
 
 # Run migrations and seed only if migrations haven't been run before
-# if [ ! -f "/var/www/html/.migrated" ]; then
-#   ./sh/wait-for-it.sh -t 30 mysql:3306 -- php artisan migrate --force
-#   php artisan migrate:refresh --seed
-#   touch /var/www/html/.migrated
-# fi
-
-./sh/wait-for-it.sh -t 30 mysql:3306 -- echo "Checking if database 'laravel' exists..."
-
-if [ -f .env ]
-then
-  export $(cat .env | sed 's/#.*//g' | xargs)
-fi
-
-if [ "$DB_CHECK" = "laravel" ]; then
-  echo "Database 'laravel' exists. Running migrations..."
-  php artisan migrate --force
+if [ ! -f "/var/www/html/.migrated" ]; then
+  ./sh/wait-for-it.sh -t 30 mysql:3306 -- php artisan migrate --force
   php artisan migrate:refresh --seed
   touch /var/www/html/.migrated
-else
-  echo "Database 'laravel' does not exist. Exiting..."
-  exit 1
 fi
-# Start the Laravel server
-php artisan serve --host=0.0.0.0 --port=8000
+
+# ./sh/wait-for-it.sh -t 30 mysql:3306 -- echo "Checking if database 'laravel' exists..."
+
+# if [ -f .env ]
+# then
+#   export $(cat .env | sed 's/#.*//g' | xargs)
+# fi
+
+# if [ "$DB_CHECK" = "laravel" ]; then
+#   echo "Database 'laravel' exists. Running migrations..."
+#   php artisan migrate --force
+#   php artisan migrate:refresh --seed
+#   touch /var/www/html/.migrated
+# else
+#   echo "Database 'laravel' does not exist. Exiting..."
+#   exit 1
+# fi
+# # Start the Laravel server
+# php artisan serve --host=0.0.0.0 --port=8000
