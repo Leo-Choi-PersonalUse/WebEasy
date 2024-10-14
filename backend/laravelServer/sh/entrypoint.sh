d2c901621c25e2667857e2558209e7427871e2b7
Leo Choi <leochoi@fseng.com.hk>
2024-10-14 10:40:15 +0800

    dev

--- backend/laravelServer/sh/entrypoint.sh
+++ backend/laravelServer/sh/entrypoint.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+cp -R /var/www/vendor /var/www/html
+
 # Run migrations and seed only if migrations haven't been run before
 if [ ! -f "/var/www/html/.migrated" ]; then
-  composer install
   ./sh/wait-for-it.sh -t 30 mysql:3306 -- php artisan migrate --force
   php artisan migrate:refresh --seed
   touch /var/www/html/.migrated
