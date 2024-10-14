0cb9fde5cf0f9fac6d93380002b59907447e2c87
Leo Choi <leochoi@fseng.com.hk>
2024-10-14 09:49:54 +0800

    dev

--- backend/laravelServer/sh/entrypoint.sh
+++ backend/laravelServer/sh/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 
 # Run migrations and seed only if migrations haven't been run before
 if [ ! -f "/var/www/html/.migrated" ]; then
+  composer install
   ./sh/wait-for-it.sh -t 30 mysql:3306 -- php artisan migrate --force
   php artisan migrate:refresh --seed
   touch /var/www/html/.migrated
@@ -26,4 +27,4 @@ fi
 #   exit 1
 # fi
 # # Start the Laravel server
-# php artisan serve --host=0.0.0.0 --port=8000
+php artisan serve --host=0.0.0.0 --port=8000
